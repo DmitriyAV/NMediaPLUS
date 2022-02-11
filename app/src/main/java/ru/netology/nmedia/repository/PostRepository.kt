60d4eeaf692290421314d6e119fa6ad895235622
DmitriyAV <dima.alekceev@gmail.com>
2022-02-11 20:18:25 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.repository
+
+import ru.netology.nmedia.dto.Post
+
+interface PostRepository {
+    fun getAll(): List<Post>
+    fun likeById(id: Long): Post
+    fun save(post: Post)
+    fun removeById(id: Long)
+    fun dislikeById(id: Long): Post
+}
